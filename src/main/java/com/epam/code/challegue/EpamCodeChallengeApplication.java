fdfd87fe6bd5a84890e1f3648e3a1abc7494991c
esteban Lopez <esteba_epam@epam.com>
2015-12-01 14:41:09 -0600

    initial commit

---
+++ src/main/java/com/epam/code/challegue/EpamCodeChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.epam.code.challegue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpamCodeChallengeApplication.class, args);
+    }
+}
